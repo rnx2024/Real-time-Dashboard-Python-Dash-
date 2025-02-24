fb17283b1e8e67a173df52e9f5ee709d1b2dd715
rnx2024 <raniurbis@gmail.com>
2025-02-25 03:12:53 +0800

    Replaced old graph layout with new graph layout for customer feedback, sentiment

--- MYSQL_Dashboard/layout.py
+++ MYSQL_Dashboard/layout.py
@@ -31,29 +31,32 @@ app.layout = dbc.Container([
                 value=1,
                 style={'fontSize': '1em'}
             ),
-        ], width=4, style={'margin-bottom': '0'})
+        ], style={'fontSize': '1em', 'padding-bottom': '1em'}
     ]),
-    dbc.Row([
-        dbc.Col([
-            html.H5("Top Customer Areas", style={'fontSize': '1.2em'}),
-            dcc.Graph(id='top-customer-areas'),
-        ], width=6, style={'padding': '1'}),
-        dbc.Col([
-            html.H5("Top Feedback", style={'fontSize': '1.2em'}),
-            dcc.Graph(id='sentiment'),
-        ], width=6, style={'padding': '1'}),
-    ], style={'margin-bottom': '0'}),
-    dbc.Row([
-        dbc.Col([
-            html.H5("Total Order Value", style={'fontSize': '1.2em', 'margin': '0'}),
-            html.Div(id='total-order-value', className="square-box", style={'padding': '1px', 'borderRadius': '1px', 'backgroundColor': '#6DA8DD', 'height': '3em'})
-        ], width=6, style={'padding': '2'}),
-        dbc.Col([
+dbc.Row([
+    dbc.Col([
+        html.H5("Top Customer Areas", style={'fontSize': '1.2em'}),
+        dcc.Graph(id='top-customer-areas'),
+    ], width=6, style={'margin-bottom': '0', 'padding-right': '2em'}),
+    dbc.Col([
+        html.H5("Feedback Category", style={'fontSize': '1.2em'}),
+        dcc.Dropdown(
+            id='feedback-category',
+            options=[
+                {'label': 'Product Quality', 'value': 'Product Quality'},
+                {'label': 'Delivery', 'value': 'Delivery'},
+                {'label': 'Customer Service', 'value': 'Customer Service'},
+                {'label': 'App Experience', 'value': 'App Experience'}
+            ],
+            placeholder='Select a feedback category',
+            value='Product Quality',
+            style={'fontSize': '1em', 'padding-bottom': '1em'}
+        ),
+
+         html.H5("Customer Feedback", style={'fontSize': '1.2em'}),
+            dcc.Graph(id='sentiment', style={'padding-bottom': '0em'}),
             html.H5("Average Delivery Time Difference", style={'fontSize': '1.2em', 'margin': '0'}),
             html.Div(id='avg-delivery-difference', className="button-3d", style={'padding': '1px', 'borderRadius': '1px', 'backgroundColor': 'green', 'boxShadow': '0px 4px 6px rgba(0, 0, 0, 0.1)', 'height': '3em'})
-        ], width=6, style={'padding': '2'}),
-    ], style={'margin-top': '0'})
+        ], width=6, style={'margin-bottom': '0', 'padding-left': '2em'})
+        ])
 ], fluid=True)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
